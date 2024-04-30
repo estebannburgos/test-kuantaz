d6260cca424cbf5728c4ec10c476e37d7dd471e4
teban burgos <esteban.n.burgos@gmail.com>
2024-04-30 03:28:49 -0400

    prueba técnica kuantaz

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ServiceApiController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/data', [ServiceApiController::class, 'index']);
